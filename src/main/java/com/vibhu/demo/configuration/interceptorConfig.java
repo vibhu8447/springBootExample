b8fe3c94d4457f902722f5d692580cc6bc29f23f
Vibhu Sharma <vibhusharma@Vibhus-MacBook-Air.local>
2023-02-02 22:00:06 +0530

    Interceptor Implemented

--- src/main/java/com/vibhu/demo/configuration/interceptorConfig.java
+++ src/main/java/com/vibhu/demo/configuration/interceptorConfig.java
@@ -1,25 +1,29 @@
 
-package configuration;
+package com.vibhu.demo.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import interceptor.GeneralInterceptor;
+import com.vibhu.demo.interceptor.GeneralInterceptor;
 
 
-@Configuration
+//@Configuration
+@Component
 public class interceptorConfig  implements WebMvcConfigurer {
 
 	@Autowired
-	private GeneralInterceptor generalInterceptor;
+	GeneralInterceptor generalInterceptor;
+	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 
-	      registry.addInterceptor(generalInterceptor).addPathPatterns("/student/**");
-	      registry.addInterceptor(generalInterceptor).addPathPatterns("/course/**");
+//	      registry.addInterceptor(generalInterceptor).addPathPatterns("/student/**");
+//	      registry.addInterceptor(generalInterceptor).addPathPatterns("/course/**");
 	      registry.addInterceptor(generalInterceptor).addPathPatterns("/**");
+//	      registry.addInterceptor(generalInterceptor);
 	      
 	   }
 }
